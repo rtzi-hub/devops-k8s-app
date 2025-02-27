3425874a85f83e343c3cc9037c5bef9a6571e668
rtzi-hub <ron.tzidky@nitzanim.tech>
2025-02-27 16:05:11 +0200

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -42,7 +42,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.devops_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/16"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
